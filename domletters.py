3df2d68d392dc835875874c6b71cfde872007d42
StevonShaw <50302094+StevonShaw@users.noreply.github.com>
2021-06-22 17:41:55 -0700

    Add functionality to open the command-line argument as a file.

--- domletters.py
+++ domletters.py
@@ -10,3 +10,13 @@ if len(sys.argv) != 2:
     usage()
     exit(1)
 
+# Open input file.
+try:
+    filename = sys.argv[1]
+    full_input_string = open(filename).read()
+except FileNotFoundError:
+    print("File:", end=" ");
+    print("'", filename, "'", sep="", end=" ")
+    print("not found.\nThis can be caused by either a mistype of the filename, or the file still needs to be created.")
+    exit(1)
+
