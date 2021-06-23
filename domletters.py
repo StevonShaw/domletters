0837d71310103936802131cff5a9f156d0e100d2
StevonShaw <50302094+StevonShaw@users.noreply.github.com>
2021-06-22 18:41:43 -0700

    Add functionality to skip invalid words.
    An invalid word is any word which contains a character outside the range a-z or A-Z, and not surrounded by whitespace or newline.

--- domletters.py
+++ domletters.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python3
 import sys
+import re
 
 def usage():
     """Outputs the correct usage of this program."""
     print("Usage:\n", sys.argv[0], "input.txt")
+
+def isValidWord(word):
+    """Checks if the word contains any characters outside [a-zA-Z].
+    
+    Args:
+        word (string): a potentially valid word.
+
+    Returns:
+        bool: is the word valid?
+    """
+    pattern = "[a-zA-Z]"
+    for char in word:
+        if not re.search(pattern, char):
+            return False
+    return True
 
 # Check for present command line argument.
 if len(sys.argv) != 2:
@@ -22,5 +38,8 @@ except FileNotFoundError:
 
 # Split the input string into words.
 words = full_input_string.split() 
+
+
 for word in words:
+    if isValidWord(word):
         print(word)
