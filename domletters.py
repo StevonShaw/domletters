a37660b2fdd3b34b7312b72ae3e0a1c4e0789ff6
StevonShaw <50302094+StevonShaw@users.noreply.github.com>
2021-06-23 18:33:14 -0700

    Update script to take input from stdin, rather than a command line arg.

--- domletters.py
+++ domletters.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-import sys
 import re
 
-def usage():
-    """Outputs the correct usage of this program."""
-    print("Usage:\n", sys.argv[0], "input.txt")
-
-def isValidWord(word):
-    """Checks if the word contains any characters outside [a-zA-Z].
+def wordContainsPattern(word, pattern):
+    """Checks if the word contains any characters outside the given pattern.
     
     Args:
         word (string): a potentially valid word.
+        pattern (string): a regex pattern string to match against. 
 
     Returns:
-        bool: is the word valid?
+        bool: does the word contain any characters outside the given pattern?
     """
-    pattern = "[a-zA-Z]"
     for char in word:
         if not re.search(pattern, char):
             return False
@@ -30,47 +25,41 @@ def countDominateLetter(word):
     Returns:
         int: the number of times the highest seen character has appeared.
     """
-    # Build map of characters to ints. Representing a letters and the number of times that letter was seen.
-    characters_seen = dict()
-    for char in word.lower():
-        if char in characters_seen:
-            characters_seen[char] += 1
-        else:
-            characters_seen[char] = 1
-    
-    # Find the highest occurance of a letter.
-    highest = 0
-    for char in characters_seen.keys():
-        if characters_seen[char] > highest:
-            highest = characters_seen[char]
-    return highest
+    try:
+        # Build map of characters to ints. Representing a letter and the number of times that letter was seen.
+        characters_seen = dict()
+        dominate_char = word.lower()[0]
+        for char in word.lower():
+            if char in characters_seen:
+                characters_seen[char] += 1
+            else:
+                characters_seen[char] = 1
+            # Update highest count seen.
+            if  characters_seen[char] > characters_seen[dominate_char]:
+                dominate_char = char
+        highest = characters_seen[dominate_char]
+    except:
+        highest = 0
+    finally:
+        return highest
 
-# Check for present command line argument.
-if len(sys.argv) != 2:
-    usage()
-    exit(1)
-
-# Open input file.
+# Read input file.
+input_string = ""
 try:
-    filename = sys.argv[1]
-    full_input_string = open(filename).read()
-except FileNotFoundError:
-    print("File:", end=" ");
-    print("'", filename, "'", sep="", end=" ")
-    print("not found.\nThis can be caused by either a mistype of the filename, or the file still needs to be created.")
-    exit(1)
+    while True:
+        input_string += input()
+        input_string += "\n"
+except EOFError:
+    pass
 
 # Split the input string into words.
-words = full_input_string.split() 
-
-# Split the input string into words.
-words = full_input_string.split() 
+words = input_string.split() 
 
 # Sum the count of each valid words' dominate letter.
 total = 0
+pattern = "[a-zA-Z]"
 for word in words:
-    if isValidWord(word):
+    if wordContainsPattern(word, pattern):
         total += countDominateLetter(word)
-
 
 print("The count of all the dominate letters in each valid word is:", total)
